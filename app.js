2f01e7171e4b8e85984be0d269adbc41600fed1c
gitihm <ismael897@outlook.com>
2019-01-15 09:34:35 +0700

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var fs = require('fs');
+var user={
+    "name":"ismael",
+    "job":"programming",
+    "age":50
+}
 http.createServer((req,res)=>{
-    res.writeHead(200,({'Content-Type':'text/html'}));
-    var myStream = fs.createReadStream(__dirname+"/"+'index.html','utf8');
-    myStream.pipe(res);
+    res.writeHead(200,({'Content-Type':'application/json'}));
+    //var myStream = fs.createReadStream(__dirname+"/"+'index.html','utf8');
+    res.end(JSON.stringify(user));
 
 }).listen(8081,'127.0.0.1');
