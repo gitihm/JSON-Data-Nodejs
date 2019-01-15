9fd127a7cc8da739572879af020e0b79bc20a006
gitihm <ismael897@outlook.com>
2019-01-15 09:29:49 +0700

    'html'

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer((req,res)=>{
-    res.writeHead(200,({'Content-Type':'text/plain'}));
-    res.end("hello world");
+    res.writeHead(200,({'Content-Type':'text/html'}));
+    var myStream = fs.createReadStream(__dirname+"/"+'index.html','utf8');
+    myStream.pipe(res);
 
 }).listen(8081,'127.0.0.1');
